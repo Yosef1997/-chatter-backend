880340f647f347005090bfd7f5d1168f0dc4fa5b
unknown <yosefsitumorang97@gmail.com>
2021-03-15 01:10:40 +0700

    create update user

---
+++ src/models/chat.js
@@ -0,0 +1,53 @@
+const db = require('../helpers/db')
+
+exports.createChat = (data = {}) => {
+  return new Promise((resolve, reject) => {
+    const query = db.query(`
+    INSERT INTO message
+    (${Object.keys(data).join()})
+    VALUES
+    (${Object.values(data).map(item => `'${item}'`).join(',')})
+    `, (err, res, field) => {
+      if (err) reject(err)
+      resolve(res)
+    })
+    console.log(query.sql)
+  })
+}
+
+exports.deleteChat = (receiver, message, sender) => {
+  return new Promise((resolve, reject) => {
+    const query = db.query(`
+  DELETE FROM message WHERE receiver=${receiver} AND message LIKE "%${message}%" AND sender=${sender}
+  `, (err, res, field) => {
+      if (err) reject(err)
+      // console.log(field)
+      resolve(res)
+    })
+    console.log(query.sql)
+  })
+}
+
+exports.getAllChat = (id) => {
+  return new Promise((resolve, reject) => {
+    const query = db.query(`
+    SELECT * from message WHERE sender=${id}
+    `, (err, res, field) => {
+      if (err) reject(err)
+      resolve(res)
+    })
+    console.log(query.sql)
+  })
+}
+
+exports.detailChat = (receiver, sender) => {
+  return new Promise ((resolve, reject) => {
+    const query = db.query(`
+    SELECT * from message WHERE receiver=${receiver} AND sender=${sender}
+    `, (err, res, field) => {
+      if (err) reject(err)
+      resolve(res)
+    }) 
+    console.log(query.sql)
+  })
+}
